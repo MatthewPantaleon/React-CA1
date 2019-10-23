11857b42fc00e1782a0b1962c2daf4e10c73c843
MatthewPantaleon <45208898+MatthewPantaleon@users.noreply.github.com>
2019-10-23 16:03:24 +0100

    Ability to parse an array of endpoints

--- src/ApiLoader.js
+++ src/ApiLoader.js
@@ -1,46 +1,71 @@
 /**
  * @Date:   2019-10-22T11:26:33+01:00
- * @Last modified time: 2019-10-22T20:22:24+01:00
+ * @Last modified time: 2019-10-23T15:59:31+01:00
  */
 
- import React from 'react';
- import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
 
 
  export default function ApiLoader(type, id){
    console.log("EXORTED");
 
    let apiEnd = "";
-   let result;
+   let results = [];
 
    // check input params
-   if(type === "civ" && id === undefined){
+   if(type === "civ" && id === undefined && typeof type === "string"){
      console.log("Get all civs");
      apiEnd = "civilizations"
-   }else if(type === "civ" && typeof id === "number"){
+   }else if(type === "civ" && typeof id === "number" && typeof type === "string"){
      console.log("Get civ with Id");
      apiEnd = `civilization/${id}`;
-   }else if(type === "unit" && id === undefined){
+   }else if(type === "unit" && id === undefined && typeof type === "string"){
      console.log("Get all units");
      apiEnd = `units`;
-   }else if(type === "unit" && typeof id === "number"){
+   }else if(type === "unit" && typeof id === "number" && typeof type === "string"){
      console.log("Get unit with Id");
-   }else if(type === "struct" && id === undefined){
+   }else if(type === "struct" && id === undefined && typeof type === "string"){
      console.log("Get all strcutures");
-   }else if(type === "struct" && typeof id === "number"){
+   }else if(type === "struct" && typeof id === "number" && typeof type === "string"){
      console.log("Get structure with Id");
    }else if(type === "tech" && id === undefined){
      console.log("Get all techs");
-   }else if(type === "tech" && typeof id === "number"){
+   }else if(type === "tech" && typeof id === "number" && typeof type === "string"){
      console.log("Get tech with id");
-   }else{
-     throw "Mssing type Parameter for API call";
+
+   }else if(type === undefined){
+     throw "Mssing type Parameter for API call. String or Array for first argument and optional id";
    }
 
 
 
+   // return fetch(`https://age-of-empires-2-api.herokuapp.com/api/v1/${apiEnd}`)
+   // .then(res => res.json());
+   if(typeof type === "string" && (id === undefined || typeof id === "number")){
+     return JSON.parse($.ajax({
+       type: "GET",
+       url: `https://age-of-empires-2-api.herokuapp.com/api/v1/${apiEnd}`,
+       async: false
+     }).responseText);
+   }else if(Array.isArray(type) && id === undefined && typeof type !== "string"){
+     
+     for(let i = 0; i < type.length; i++){
+       $.ajax({
+         type: "GET",
+         url: type[i],
+         async: false,
+         dataType: "JSON",
+         success(data){
+           results.push(data);
+         }
+       });
+     }
+     console.log(results);
+     return results;
+   }
 
-   return fetch(`https://age-of-empires-2-api.herokuapp.com/api/v1/${apiEnd}`)
-   .then(res => res.json()).then((e) => result = e);
+
 
  };
--- src/components/civ/CivComponent.js
+++ src/components/civ/CivComponent.js
@@ -1,6 +1,6 @@
 /**
  * @Date:   2019-10-22T18:41:31+01:00
- * @Last modified time: 2019-10-22T20:28:45+01:00
+ * @Last modified time: 2019-10-23T16:02:38+01:00
  */
 
 import React, {Component} from 'react';
@@ -15,14 +15,26 @@ import ApiLoader from '../../ApiLoader';
 
      this.state = {
        data: null,
-       selectedCiv: {civilization_bonus: []}
+       selectedCiv: {civilization_bonus: []},
+       unique_unit: [],
+       unique_tech: []
      };
 
 
    }
 
    componentDidMount(){
-     ApiLoader("civ").then(response => this.setState({ data: response.data}));
+     this.setState({data: ApiLoader("civ")}, () =>
+      this.setState({selectedCiv: this.state.data.civilizations[0]}, () => {
+        console.log(this.state);
+        this.setState({unique_tech: ApiLoader(this.state.selectedCiv.unique_tech)}, ()=> console.log(this.state));
+      }
+    ));
+
+   }
+
+   changeValue(e){
+     this.setState({selectedCiv: this.state.data.civilizations[parseInt(e.target.value)]});
    }
 
    render(){
@@ -31,7 +43,24 @@ import ApiLoader from '../../ApiLoader';
      }
 
      return(
-       <p>CIVS</p>
+       <>
+       <select className="form-control" onChange={(e) => this.changeValue(e)}>
+       {this.state.data.civilizations.map((e, i) => <option value={i} key={e.id}>{e.name}</option>)}
+       </select>
+
+       <hr />
+
+       <ul>
+       {this.state.selectedCiv.civilization_bonus.map((e, i) => <li key={i}>{e}</li>)}
+       </ul>
+
+       <hr />
+
+       <ul>
+       {this.state.unique_tech.map((e, i) => <li key={i}>{e.name}</li>)}
+       </ul>
+
+       </>
      );
    }
  }
